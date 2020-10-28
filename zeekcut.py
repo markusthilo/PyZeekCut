7ee0c9ea0a548147da15a57bb65f0f98b35af0dc
Markus Thilo <markus.thilo@gmail.com>
2020-10-28 13:40:51 +0100

    Version 0.4: added method >run<

--- zeekcut.py
+++ zeekcut.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Markus Thilo'
-__version__ = '0.3-20200402'
+__version__ = '0.4-20201028'
 __license__ = 'GPL-3'
 __help__ = '''
 Wrapper for zeek-cut
 This is executed: cat <logfiles> | zeek-cut [<options>] <columns>
 Usage / example:
-zeeklog = ZeekCut(
+zeekcut = ZeekCut()
+zeeklog = zeekcut.run(
 	logfiles,
 	columns = ['ts', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p'],
 	options = '-d'
@@ -21,6 +22,7 @@ print(zeeklog.json)
 print(zeeklog.data)
 '''
 
+from os import environ, path, access, X_OK
 from subprocess import Popen, PIPE
 from ipaddress import ip_address
 from json import dumps
@@ -28,32 +30,41 @@ from json import dumps
 class ZeekCut:
 	'Python wrapper for zeek-cut'
 
-	ZEEKCUT = '/usr/local/zeek/bin/zeek-cut'	# set path to zeek-cut here!!!
+	PATHS = [	# possible paths to look for zeek-cut executable
+		'/usr/local/zeek/bin',
+		'/opt/zeek/bin',
+		'/usr/local/bin',
+		'/usr/bin'
+	]
+	ZEEKCUT = 'zeek-cut'	# define command for zeek-cut
+	CAT = 'cat'	# define command for cat
 
-	def __init__(self,
+	def __init__(self, zeekcut=None, cat=None):
+		'Create Object for zeek-cut'
+		if zeekcut == None:
+			for directory in environ['PATH'].split(':') + self.PATHS:
+				self.zeekcut = path.join(directory, self.ZEEKCUT)
+				if path.isfile(self.zeekcut) and access(self.zeekcut, X_OK):
+					break
+		else:
+			self.zeekcut = zeekcut
+		if cat == None:
+			self.cat = self.CAT
+		else:
+			self.cat = cat
+		
+	def run(self,
 		logfiles,
 		columns = ['ts', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p'],
 		options = None
 	):
-		'''
-			Create Object for zeek-cut:
-			cat logfile | zeek-cut [<options>] <columns>
-		'''
-		self.logfiles = []	# create a list of filenames as strings
-		if isinstance(logfiles, str):
-			self.logfiles = [logfiles]
-		else:
-			for logfile in logfiles:
-				if isinstance(logfile, str):
-					self.logfiles.append(logfile)
-				else:
-					self.logfiles.append(logfile.name)
-		cat_cmd = ['cat']	# assemble shell command for cat
-		cat_cmd.extend(self.logfiles)
-		zeek_cmd = [self.ZEEKCUT]	# assemble shell command for zeek-cut
-		if options != None:
+		'Execute cat logfile | zeek-cut [<options>] <columns>'
+		cat_cmd = [self.cat]	# assemble shell command for cat
+		cat_cmd.extend(self.__convert2list__(logfiles))
+		zeek_cmd = [self.zeekcut]
+		if options != None:	# assemble shell command for zeek-cut
 			zeek_cmd.extend(self.__convert2list__(options))
-		zeek_cmd.extend(self.__convert2list__(columns))
+		zeek_cmd.extend(columns)
 		cat = Popen(cat_cmd, stdout=PIPE)	# generate cat
 		zeekcut = Popen(zeek_cmd, stdin=cat.stdout, stdout=PIPE)	# generate zeek-cut
 		lines = zeekcut.communicate()[0].decode().rstrip('\n').split('\n')	# read lines from stdout
